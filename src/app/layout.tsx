1e3957a8a7b62aa43ac7c1afdd386ef9d476e4f7
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-06-26 13:03:44 +0300

    Add sticky header.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import { StoreProvider } from "@/redux/StoreProvider";
 import "./globals.css";
+import style from "./layout-styles.module.css";
 
 import { Roboto } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -19,7 +22,20 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${roboto.variable}`}>
       <body>
-        <StoreProvider>{children}</StoreProvider>
+        <header className={style.header}>
+          <h1 className={style.siteName}>Билетопоиск</h1>
+          <Link href="#">
+            <Image
+              src="/icons/basket.svg"
+              width="32"
+              height="32"
+              alt="корзина"
+            />
+          </Link>
+        </header>
+        <div className={style.pageContent}>
+          <StoreProvider>{children}</StoreProvider>
+        </div>
       </body>
     </html>
   );
