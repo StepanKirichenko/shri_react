ccb61708cced9fb2d163567aa0b6cbe35d8269b4
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-06-26 13:56:25 +0300

    Add tickert counter to header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,36 @@
 import Link from "next/link";
 import Image from "next/image";
 import style from "./style.module.css";
+import { useSelector } from "react-redux";
+import { selectTicketTotalAmount } from "@/redux/features/cart/selector";
+import { State } from "@/redux/store";
+
+function TicketCounter() {
+  const ticketAmount = useSelector((state: State) =>
+    selectTicketTotalAmount(state)
+  );
+
+  if (ticketAmount === 0) {
+    return <></>;
+  }
+
+  return (
+    <div className={style.ticketCounter}>
+      <p>{ticketAmount}</p>
+    </div>
+  );
+}
 
 export default function Header() {
   return (
     <header className={style.header}>
       <h1 className={style.siteName}>Билетопоиск</h1>
-      <Link href="/">
-        <Image src="/icons/basket.svg" width="32" height="32" alt="корзина" />
-      </Link>
+      <div className={style.cart}>
+        <TicketCounter />
+        <Link href="/">
+          <Image src="/icons/basket.svg" width="32" height="32" alt="корзина" />
+        </Link>
+      </div>
     </header>
   );
 }
--- src/redux/features/cart/selector.ts
+++ src/redux/features/cart/selector.ts
@@ -4,3 +4,13 @@ export const selectCartModule = (state: State) => state.cart;
 
 export const selectTicketAmount = (state: State, id: string) =>
   selectCartModule(state)[id] || 0;
+
+export const selectTicketTotalAmount = (state: State) => {
+  const cart = selectCartModule(state);
+  const movieIds = Object.getOwnPropertyNames(cart);
+  let count = 0;
+  for (const movieId of movieIds) {
+    count += cart[movieId];
+  }
+  return count;
+};
