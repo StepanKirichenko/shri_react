4b2d39ebb56b1d0de79eb81f574ea9b88631ee70
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-06-26 15:00:51 +0300

    Add filter element mockups.

--- src/components/FiltersPanel/FiltersPanel.tsx
+++ src/components/FiltersPanel/FiltersPanel.tsx
@@ -3,9 +3,19 @@ import style from "./style.module.css";
 export function FiltersPanel() {
   return (
     <div className={style.panel}>
-      <p>First</p>
-      <p>Second</p>
-      <p>Third</p>
+      <h2 className={style.panelHeading}>Фильтры поиска</h2>
+      <div className={style.filter}>
+        <h3 className={style.filterName}>Название</h3>
+        <input placeholder="Введите название" />
+      </div>
+      <div className={style.filter}>
+        <h3 className={style.filterName}>Жанр</h3>
+        <input placeholder="Введите название" />
+      </div>
+      <div className={style.filter}>
+        <h3 className={style.filterName}>Кинотеатр</h3>
+        <input placeholder="Введите название" />
+      </div>
     </div>
   );
 }
