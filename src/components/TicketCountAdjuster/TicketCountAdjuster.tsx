fe506bb8c8c0dc143e7dc773aa99c90f10390055
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-06-27 04:37:54 +0300

    Diable add/remove ticket buttons.

--- src/components/TicketCountAdjuster/TicketCountAdjuster.tsx
+++ src/components/TicketCountAdjuster/TicketCountAdjuster.tsx
@@ -7,26 +7,24 @@ import { State } from "@/redux/store";
 import { cartActions } from "@/redux/features/cart";
 import Image from "next/image";
 
-function TicketCount({ movieId }: { movieId: string }) {
+export default function TicketCountAdjuster({ movieId }: { movieId: string }) {
   const count = useSelector((state: State) =>
     selectTicketAmount(state, movieId)
   );
-  return <p className={style.ticketCount}>{count}</p>;
-}
-
-export default function TicketCountAdjuster({ movieId }: { movieId: string }) {
   const dispath = useDispatch();
 
   return (
     <div className={style.container}>
       <button
+        disabled={count === 0}
         className={style.button}
         onClick={() => dispath(cartActions.decrement(movieId))}
       >
         <Image src="/icons/minus.svg" width="12" height="12" alt="decrease" />
       </button>
-      <TicketCount movieId={movieId} />
+      <p className={style.ticketCount}>{count}</p>
       <button
+        disabled={count >= 30}
         className={style.button}
         onClick={() => dispath(cartActions.increment(movieId))}
       >
